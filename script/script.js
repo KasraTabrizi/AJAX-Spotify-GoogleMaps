8b6b6eca8b6a7ba3c0527e8279129b54d11e0c51
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-11-21 11:27:59 +0100

    transfered script for webplay SDK from index html to the script.js

--- script/script.js
+++ script/script.js
@@ -1,24 +1,122 @@
 let countryID = ``;
 let urlPlaylist = `https://api.spotify.com/v1/playlists/${countryID}`;
-const spotifyKey = `BQDAxxkdlEw8HLxSqzU5QOWJdx15Uo8censrvgo7FSAF1ChvT5dNYPSR2hAYsKoXE5dVbhRwZMJGbYXB9PV3viJHocIFbC7hwTpPy0ZLne8tuFmhPV10MPeZwBlFhHCzZzfnCapvmGV_EGUuV-P5B4rSppEgUt9Ewok`;
+const spotifyKey = `BQDOf_dxTMXFBqg3MTQB_U4e2LPffhxxcVYmePh3KVnUTCmr3PW1SG8RSnWfOwWUlOy_euVD_GWi7GCmkBuqoic92cJBw41s9kO7ybQCrRIlFU4nGzIJh5A9Z__VNDOMJ96IUhDyAgGKSoGFqeAeYQMMHj7uAvE1Q7g`;
 const mapboxKey = `pk.eyJ1Ijoia2FzcmF0YWJyaXppIiwiYSI6ImNrMzdmNGxhbTBhdmkzbHFlNm4zNzM1MXIifQ.NTIDE9lmvt_g4IY_U2Rw6w`;
 
-// document.getElementById("button").addEventListener("click", () => {
-//     fetch(urlPlaylist, {
-//             method: "GET",
-//             headers: {
-//                 "Authorization": `Bearer ${spotifyKey}`,
-//                 "Accept": "application/json",
-//                 "Content-Type": "application/json"
-//             },
-//             credentials: "same-origin"
-//         })
-//         .then(res => res.json())
-//         .then(musicList => {
-//             console.log(musicList);
-//         });
+window.onSpotifyWebPlaybackSDKReady = () => {
+    const token = spotifyKey;
+    const player = new Spotify.Player({
+        name: 'Kasra Tabrizi',
+        getOAuthToken: cb => {
+            cb(token);
+        }
+    });
 
-// });
+    // Error handling
+    player.addListener('initialization_error', ({
+        message
+    }) => {
+        console.error(message);
+    });
+    player.addListener('authentication_error', ({
+        message
+    }) => {
+        console.error(message);
+    });
+    player.addListener('account_error', ({
+        message
+    }) => {
+        console.error(message);
+    });
+    player.addListener('playback_error', ({
+        message
+    }) => {
+        console.error(message);
+    });
+
+    // Playback status updates
+    player.addListener('player_state_changed', state => {
+        console.log(state);
+    });
+
+    // Not Ready
+    player.addListener('not_ready', ({
+        device_id
+    }) => {
+        console.log('Device ID has gone offline', device_id);
+    });
+    ////////////////////////////////////////////////////////////////////////////////////////
+    // Called when connected to the player created beforehand successfully
+    player.addListener('ready', ({
+        device_id
+    }) => {
+        console.log('Ready with Device ID', device_id);
+
+        const play = ({
+            spotify_uri,
+            playerInstance: {
+                _options: {
+                    getOAuthToken,
+                    id
+                }
+            }
+        }) => {
+            getOAuthToken(access_token => {
+                fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                        uris: [spotify_uri]
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${access_token}`
+                    },
+                });
+            });
+        };
+        const pause = ({
+            spotify_uri,
+            playerInstance: {
+                _options: {
+                    getOAuthToken,
+                    id
+                }
+            }
+        }) => {
+            getOAuthToken(access_token => {
+                fetch(`https://api.spotify.com/v1/me/player/pause?device_id=${id}`, {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                        uris: [spotify_uri]
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${access_token}`
+                    },
+                });
+            });
+        };
+        document.getElementById("buttonPlay").addEventListener("click", () => {
+            play({
+                playerInstance: player,
+                spotify_uri: 'spotify:track:1rgnBhdG2JDFTbYkYRZAku',
+            });
+
+        });
+        document.getElementById("buttonStop").addEventListener("click", () => {
+            pause({
+                playerInstance: player,
+                spotify_uri: 'spotify:track:1rgnBhdG2JDFTbYkYRZAku',
+            });
+
+        });
+
+    });
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // Connect to the player!
+    player.connect();
+
+};
 
 mapboxgl.accessToken = mapboxKey;
 var map = new mapboxgl.Map({
@@ -57,10 +155,12 @@ map.on('click', function(e) {
     }
     var feature = features[0];
 
+
     console.log(feature.properties.name);
     console.log(getCountryId(countryList, feature.properties.name));
     countryID = getCountryId(countryList, feature.properties.name);
     urlPlaylist = `https://api.spotify.com/v1/playlists/${countryID}`;
+
     fetch(urlPlaylist, {
             method: "GET",
             headers: {
@@ -75,11 +175,9 @@ map.on('click', function(e) {
             console.log(musicList);
         });
 
-
-
 });
 
-
+//return the country spotify ID by passing the countrylist array and the selected country fetched from the mapbox API
 function getCountryId(countryList, selectedCountry) {
     for (let i = 0; i < countryList.length; i++) {
         if (selectedCountry == countryList[i].country) {
