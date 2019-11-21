e2b2e3f87b3bad6770e91a644cebb49dc424d40b
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-11-21 09:54:59 +0100

    when you click on the country, it will give the top 50 hit list of that country via the spotify API

---
+++ script/country_id.js
@@ -0,0 +1,61 @@
+const countryList = [{
+        country: "Argentina",
+        id: "37i9dQZEVXbMMy2roB9myp"
+    },
+    {
+        country: "Australia",
+        id: "37i9dQZEVXbJPcfkRz0wJ0"
+    },
+    {
+        country: "Belgium",
+        id: "37i9dQZEVXbJNSeeHswcKB"
+    },
+    {
+        country: "Bolivia",
+        id: "37i9dQZEVXbJqfMFK4d691"
+    },
+    {
+        country: "Brazil",
+        id: "37i9dQZEVXbMXbN3EUUhlg"
+    },
+    {
+        country: "Bulgaria",
+        id: "37i9dQZEVXbNfM2w2mq1B8"
+    },
+    {
+        country: "Canada",
+        id: "37i9dQZEVXbKj23U1GF4IR"
+    },
+    {
+        country: "Chile",
+        id: "37i9dQZEVXbL0GavIqMTeb"
+    },
+    {
+        country: "Colombia",
+        id: "37i9dQZEVXbOa2lmxNORXQ"
+    },
+    {
+        country: "Costa Rica",
+        id: "37i9dQZEVXbMZAjGMynsQX"
+    },
+    {
+        country: "Denmark",
+        id: "37i9dQZEVXbL3J0k32lWnN"
+    },
+    {
+        country: "Dominican Rep.",
+        id: "37i9dQZEVXbKAbrMR8uuf7"
+    },
+    {
+        country: "Germany",
+        id: "37i9dQZEVXbJiZcmkrIHGU"
+    },
+    {
+        country: "Ecuador",
+        id: "37i9dQZEVXbJlM6nvL1nD1"
+    },
+    {
+        country: "El Salvador",
+        id: "37i9dQZEVXbLxoIml4MYkT"
+    }
+];
--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
-const countryID = `37i9dQZEVXbJNSeeHswcKB`
-const urlPlaylist = `https://api.spotify.com/v1/playlists/${countryID}`;
+let countryID = ``;
+let urlPlaylist = `https://api.spotify.com/v1/playlists/${countryID}`;
 //const url = `https://api.spotify.com/v1/browse/featured-playlists?country=US&limit=2`;      
 //const urlAuth = `https://accounts.spotify.com/authorize?client_id=edb6db7c1c604795b872fe40255d52fc&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1:5500`;
-const spotifyKey = `BQCvMwYLYfIHhJPQYQEu2qYMeUVp4uLUcXh_25YnR6QhXBLkjfRRZsr09w4oVs14ekuEUM1HtkzBvQBJ2XBffwAiY9PoCW2tMaLCrRJKr5X-9XUsZFG2GBGTsO42eZluho3QJm0DMmEcdNYq0pSsidBbXrAXyjcRa77PtpBEQxJ_j4VIkmrVkDuVcpmzJDB3O5GgpZK0L8b5dqbMITGR1Wqr4I6HS23c01Hwb0SxtjSzV7DsFjZLmBmkfzOq5_QodOPVX6vqwGjOrA`;
+const spotifyKey = `BQC9MGgxpyeiIbKC1hTk8VxlsxVcxfDJV57hOD6tL7QNYAmn3ZFDrfqykTsfU4AhhBT_QeAQgRGSzGkepdKi-xnlGwEXv9wjvr-daIxS75Ihd2CeTQitd-66KtyKhvOrno1dSq-CRr20EfP2tB-h6qFbG78qoAnmGHXzy_Fwz2asgbALN1pWnxKiuoM0B-OUmN4_rVzRPKedPu9awHxP7P2RJUKRbZg7sBwxajisAA0GUoxbqwBn_NUpo3WUd6Nz-lt_k64uFtriug`;
 const mapboxKey = `pk.eyJ1Ijoia2FzcmF0YWJyaXppIiwiYSI6ImNrMzdmNGxhbTBhdmkzbHFlNm4zNzM1MXIifQ.NTIDE9lmvt_g4IY_U2Rw6w`;
 
 document.getElementById("button").addEventListener("click", () => {
@@ -60,4 +60,32 @@ map.on('click', function(e) {
     var feature = features[0];
 
     console.log(feature.properties.name);
+    console.log(getCountryId(countryList, feature.properties.name));
+    countryID = getCountryId(countryList, feature.properties.name);
+    urlPlaylist = `https://api.spotify.com/v1/playlists/${countryID}`;
+    fetch(urlPlaylist, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${spotifyKey}`,
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            credentials: "same-origin"
+        })
+        .then(res => res.json())
+        .then(musicList => {
+            console.log(musicList);
+        });
+
+
+
 });
+
+
+function getCountryId(countryList, selectedCountry) {
+    for (let i = 0; i < countryList.length; i++) {
+        if (selectedCountry == countryList[i].country) {
+            return countryList[i].id;
+        }
+    }
+}
